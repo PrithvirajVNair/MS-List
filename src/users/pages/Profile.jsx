bdae4af5655c8dc859e8d2a1f6a76df386cec05f
PrithvirajVNair <prithvirajvnair.pvr@gmail.com>
2025-11-15 21:00:42 +0530

    Profile

--- src/users/pages/Profile.jsx
+++ src/users/pages/Profile.jsx
@@ -9,13 +9,13 @@ const Profile = () => {
     <>
         <Header/>
         <div className='bg-black min-h-screen text-white'>
-            <div className='sm:grid grid-cols-7 min-h-screen place-content-center'>
+            <div className='lg:grid grid-cols-7 min-h-screen place-content-center'>
                 <div></div>
                 <div className='col-span-5 p-10 rounded bg-white/10'>
-                    <div className='sm:grid grid-cols-5'>
+                    <div className='lg:grid grid-cols-5'>
                         <div className='relative flex flex-col justify-center items-center'>
                             <div className='flex'>
-                                <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="no image" className='me-5 p-3' style={{ widows: '150px', height: '150px', borderRadius: '50%' }}/>
+                                <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="no image" className='p-3' style={{ widows: '150px', height: '150px', borderRadius: '50%' }}/>
                                 <label  htmlFor="edit-profile" className='absolute right-0 cursor-pointer'><FontAwesomeIcon icon={faPenToSquare} />
                                 <input type="file" id='edit-profile' className='hidden' />
                                 </label>
@@ -23,7 +23,7 @@ const Profile = () => {
                             <h2 className='text-start'>Username</h2>
                         </div>
                         <div className='col-span-2 text-center text-blue-200'>
-                            <h1 className='text-white underline mb-5'>Status</h1>
+                            <h1 className='text-white underline mb-5 text-2xl py-2'>Status</h1>
                             <Link to={'/watchlist'}><p>Total :</p></Link>
                             <Link to={'/watchlist/watching'}><p>Watching :</p></Link>
                             <Link to={'/watchlist/completed'}><p>Completed :</p></Link>
@@ -32,7 +32,7 @@ const Profile = () => {
                             <Link to={'/watchlist/planning'}><p>Planning :</p></Link>
                         </div>
                         <div className='col-span-2 text-center'>
-                            <h1 className='text-white underline mb-5'>Recently Added</h1>
+                            <h1 className='text-white underline mb-5 text-2xl py-2'>Recently Added</h1>
                             <div className='h-[200px] overflow-hidden'>
                                 <div className='flex rounded-xl bg-white/20 h-[100px]'>
                                     <div className='h-[100px] w-20'>
