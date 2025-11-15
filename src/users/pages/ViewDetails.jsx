b40bb8824e97c2ace26b9aeb857148e54a5d6627
PrithvirajVNair <prithvirajvnair.pvr@gmail.com>
2025-11-16 02:24:52 +0530

    Responsive Details

--- src/users/pages/ViewDetails.jsx
+++ src/users/pages/ViewDetails.jsx
@@ -44,13 +44,13 @@ const ViewDetails = () => {
         <>
             <Header />
             <div className='min-h-screen bg-black sm:px-10 text-white sm:pt-20'>
-                <div className='flex h-[400px] p-5 bg-white/10'>
-                    <div className='min-w-[250px] h-full'>
-                        <img className='w-full h-full object-fill' src="https://i.pinimg.com/originals/89/3f/fd/893ffdb8c9e5d47e2855e348848195b9.jpg" alt="" />
+                <div className='flex justify-center items-center max-sm:flex-col min-h-[400px] p-5 bg-white/10'>
+                    <div className='sm:min-w-[250px] w-[250px] h-full sm:pt-0 pt-10'>
+                        <img className='w-full h-full object-fill p-5 sm:p-0' src="https://i.pinimg.com/originals/89/3f/fd/893ffdb8c9e5d47e2855e348848195b9.jpg" alt="" />
                     </div>
-                    <div className='px-5'>
-                        <h1 className='text-3xl'>TITLE</h1>
-                        <div className='h-[200px] overflow-y-auto'><p><span className='text-blue-300'>Description: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.</p>
+                    <div className='sm:px-5'>
+                        <h1 className='text-xl sm:text-3xl'>TITLE</h1>
+                        <div className='h-[200px] overflow-y-auto sm:text-base text-sm'><p><span className='text-blue-300'>Description: </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.or sit amet consectetur adipisicing elit. Nulla ut praesentium nesciunt tenetur totam numquam perspiciatis tempore ipsam placeat, beatae dolor officia iste facere necessitatibus laborum magni molestiae veritatis? Corrupti.</p>
                         </div>
                         <div className='p-3'>
                             <span className='bg-black/60 rounded-2xl px-2 text-sm me-2'>Action</span>
@@ -59,7 +59,7 @@ const ViewDetails = () => {
                         <div className='flex justify-between'>
                             <div className='flex'>
                                 <div>
-                                    <button onClick={() => setToggleList(true)} className='me-10 text-base py-2 px-5 rounded-xl bg-linear-to-r via-[#000CF1]/60 hover:via-[#000CF1] via-30% from-[#000CF1]/60 hover:from-[#000CF1] to-black/60 hover:to-black text-white cursor-pointer'>Add to List</button>
+                                    <button onClick={() => setToggleList(true)} className='me-10 text-xs sm:text-base py-2 px-5 rounded-xl bg-linear-to-r via-[#000CF1]/60 hover:via-[#000CF1] via-30% from-[#000CF1]/60 hover:from-[#000CF1] to-black/60 hover:to-black text-white cursor-pointer'>Add to List</button>
                                 </div>
                                 {/* { toggleList &&
                                 <div className='flex flex-col border bg-blue-300/10 text-center rounded backdrop-blur-2xl px-5 py-3'>
@@ -70,22 +70,22 @@ const ViewDetails = () => {
                                 <span className='hover:text-blue-300 cursor-pointer'>Dropped</span>
                             </div>} */}
                             </div>
-                            <p className='text-white/60 me-2 mt-1 text-xl ps-5'>Rating: <FontAwesomeIcon icon={faStar} className='me-1 text-yellow-400' />9.5/10</p>
+                            <p className='text-white/60 me-2 mt-1 text-sm sm:text-xl ps-5'>Rating: <FontAwesomeIcon icon={faStar} className='me-1 text-yellow-400' />9.5/10</p>
                         </div>
                     </div>
                 </div>
                 {/* comments */}
-                <h1 className='mt-10 text-2xl'>Comments:</h1>
-                <div className='flex flex-col min-h-[400px] p-10 border border-white/20'>
+                <h1 className='mt-10 text-base sm:px-0 px-5 sm:text-2xl'>Comments:</h1>
+                <div className='flex flex-col min-h-[400px] p-5 sm:p-10 border border-white/20'>
                     <div className='flex justify-start items-start w-full'>
-                        <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="no image" className='me-5' style={{ widows: '40px', height: '40px', borderRadius: '50%' }} />
-                        <input type="text" className='bg-white/10 py-1 px-2 text-white w-full rounded-xl placeholder:text-white/60' placeholder='Write a comment' />
+                        <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="no image" className='me-5 sm:w-10 sm:h-10 w-8 h-8' style={{ borderRadius: '50%' }} />
+                        <input type="text" className='bg-white/10 py-1 px-2 text-white w-full rounded-xl placeholder:text-white/60 sm:text-base text-sm' placeholder='Write a comment' />
                     </div>
                     <div className='mt-10 flex'>
-                        <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="no image" className='me-3' style={{ widows: '40px', height: '40px', borderRadius: '50%' }} />
+                        <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="no image" className='me-3 sm:w-10 sm:h-10 w-8 h-8' style={{ borderRadius: '50%' }} />
                         <div className='flex flex-col'>
-                            <h5>Name</h5>
-                            <p className='mt-3'>hello</p>
+                            <h5 className='sm:text-base text-sm'>Name</h5>
+                            <p className='mt-3 sm:text-base text-sm'>hello</p>
                         </div>
                     </div>
                 </div>
