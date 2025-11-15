0d4b7f44b639b5acb92851d3da97b71ea4a45744
PrithvirajVNair <prithvirajvnair.pvr@gmail.com>
2025-11-16 02:44:18 +0530

    Category

--- src/users/pages/Category.jsx
+++ src/users/pages/Category.jsx
@@ -6,7 +6,7 @@ const Category = () => {
     <>
         <Header category/>
         <div className='bg-black min-h-screen text-white'>
-            <div className='sm:p-20 sm:pt-0 sm:px-0 px-5 pt-20'>
+            <div className='sm:px-20 px-5 pt-20'>
                 <h2 className='text-xl sm:text-3xl mb-5'>Language:</h2>
                 <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
                     <div className='sm:m-5 m-1 sm:h-[100px] h-10 rounded-xl flex justify-center items-center bg-white/10 hover:bg-blue-300/10 hover:text-blue-300'>
@@ -26,8 +26,8 @@ const Category = () => {
                     </div>
                 </div>
             </div>
-            <div className='sm:p-20 sm:pt-0 sm:px-0 px-5 pt-5'>
-                <h2 className='text-xl sm:text-3xl mb-5'>Language:</h2>
+            <div className='sm:px-20 sm:pt-0 px-5 pt-5'>
+                <h2 className='text-xl sm:text-3xl mb-5'>Genre:</h2>
                 <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
                     <div className='sm:m-5 m-1 sm:h-[100px] h-10 rounded-xl flex justify-center items-center bg-white/10 hover:bg-blue-300/10 hover:text-blue-300'>
                         <p className='sm:text-2xl text-sm'>Action</p>
