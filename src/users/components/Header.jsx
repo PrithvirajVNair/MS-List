41956f143f5d757aeb91dda77b0cd60dd5581358
PrithvirajVNair <prithvirajvnair.pvr@gmail.com>
2025-11-21 19:37:07 +0530

    Feedback

--- src/users/components/Header.jsx
+++ src/users/components/Header.jsx
@@ -33,7 +33,7 @@ const Header = ({ home, watchlist, category, search, feedback }) => {
                     }} />
                 </div>
 
-                
+
                 {/* following is the content of header for big screen */}
                 <ul className='hidden md:flex items-center sm:text-2xl'>
                     {search ? <li className={`pe-10 hover:text-blue-300 ${search && 'text-blue-300'}`}><a href='#'><FontAwesomeIcon icon={faMagnifyingGlass} /> Search</a></li> :
@@ -70,6 +70,8 @@ const Header = ({ home, watchlist, category, search, feedback }) => {
                                 <li className={`hover:text-blue-300 ${category && 'text-blue-300'}`}><a href='/category'>Category</a></li>}
                             {watchlist ? <li className={`hover:text-blue-300 ${watchlist && 'text-blue-300'}`}><a href='#'>Watchlist</a></li> :
                                 <li className={`hover:text-blue-300 ${watchlist && 'text-blue-300'}`}><a href='/watchlist'>Watchlist</a></li>}
+                            {feedback ? <li className={`pe-10 hover:text-blue-300 ${feedback && 'text-blue-300'}`}><a href='#'><FontAwesomeIcon icon={faBug} className='me-1' />Report</a></li> :
+                                <li className={`pe-10 hover:text-blue-300 ${feedback && 'text-blue-300'}`}><a href='/feedback'><FontAwesomeIcon icon={faBug} className='me-1' />Report</a></li>}
                             {/* {watchlist && <button className='sm:text-base text-xs mt-2 py-2 px-5 rounded-xl bg-linear-to-r via-[#000CF1]/60 hover:via-[#000CF1] via-30% from-[#000CF1]/60 hover:from-[#000CF1] to-black/60 hover:to-black text-white cursor-pointer'>Add to List</button>} */}
                         </ul>
                     </div>
