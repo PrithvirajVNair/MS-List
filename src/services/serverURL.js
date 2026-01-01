cf04410640e77d7ec8e6b30ccd7d8671f5dfe07a
PrithvirajVNair <prithvirajvnair.pvr@gmail.com>
2026-01-01 18:26:39 +0530

    FIX : Other User profile to Current User profile reload

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,2 @@
-export const serverURL = "http://localhost:4000" 
-// export const serverURL = "https://ms-list-server.onrender.com"
+// export const serverURL = "http://localhost:4000" 
+export const serverURL = "https://ms-list-server.onrender.com"
